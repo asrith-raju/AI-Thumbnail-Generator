f857e0dc944a13acea3efd7fc7f8e1e4d49c3f9b
asrith-raju <asrithraju143@gmail.com>
2026-01-27 21:25:35 +0530

    server(feat): add logout and verify routes to AuthRouter

--- server/routes/AuthRoutes.ts
+++ server/routes/AuthRoutes.ts
@@ -1,7 +1,12 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/AuthController.js";
+import { loginUser, logoutUser, registerUser, verifyUser } from "../controllers/AuthController.js";
+import protect from "../middlewares/auth.js";
 
 const AuthRouter = express .Router();
 
 AuthRouter.post('/register',registerUser)
 AuthRouter.post('/login',loginUser)
+AuthRouter.get('/verify',protect,verifyUser)
+AuthRouter.post('/logout',logoutUser)
+
+export default AuthRouter;
