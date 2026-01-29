77a90b0fd2f45b69b130d405f54e5492cd4c107f
asrith-raju <asrithraju143@gmail.com>
2026-01-29 11:20:16 +0530

    server(feat): add style prompts for thumbnail generation

--- server/controllers/ThumbnailController.ts
+++ server/controllers/ThumbnailController.ts
@@ -1,6 +1,26 @@
 import { Request, Response } from 'express';
 import Thumbnail from '../models/Thumbnail.js';
+import { GenerateContentConfig, HarmBlockThreshold, HarmCategory } from '@google/genai';
+import ai from '../configs/ai.js';
+import path from 'path';
+import fs from 'fs';
 
+
+const stylePrompts = {
+  'Bold and Graphic': 'eye-catching thumbnail, bold typography, vibrant colors, expressive facial reaction, dramatic lighting, high contrast, click-worthy composition, professional design',
+
+  'Tech/Futuristic': 'futuristic thumbnail, sleek modern design, digital UI elements, glowing accents, holographic effects, cyber-tech aesthetic, sharp lighting, high-tech atmosphere',
+
+  'Minimalist': 'minimalist thumbnail, clean layout, simple shapes, limited color palette, plenty of negative space, modern flat design, clear focal point',
+
+  'Photorealistic': 'photorealistic thumbnail, ultra-realistic lighting, natural skin tones, candid moment, DSLR-style photography, lifestyle realism, shallow depth of field',
+
+  'Illustrated': 'illustrated thumbnail, custom digital illustration, stylized characters, bold outlines, vibrant colors, creative cartoon or vector art style'
+};
+
+
+
+ 
 
 export const generateThumbnail = async (req:Request, res:Response) => {
      try {
@@ -17,7 +37,7 @@ export const generateThumbnail = async (req:Request, res:Response) => {
             isGenerating:true
         })
          
-     } catch (error) {
-        
+     }catch(error:any){
+
      }
-}
+} 
