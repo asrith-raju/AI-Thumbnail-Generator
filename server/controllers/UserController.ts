8fb4e48e15c3b29a321b4b6b93582506c181cbd5
asrith-raju <asrithraju143@gmail.com>
2026-01-29 12:10:22 +0530

    server(feat): add getThumbnailById controller for fetching thumbnail by ID

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -15,3 +15,15 @@ export const getUserThumbnails = async (req:Request, res:Response) => {
     }
 } 
 
+export const getThumbnailById = async (req:Request, res:Response) => {
+    try {
+        const {userId} = req.session;
+        const id = req.params
+
+        const thumbnail = await Thumbnail.findOne({_id:id,userId});
+        res.json({thumbnail})
+    } catch (error:any) {
+        console.log(error); 
+        res.status(500).json({message:error.message});
+    }
+} 
